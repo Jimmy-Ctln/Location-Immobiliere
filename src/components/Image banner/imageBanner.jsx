929891d3a0ae566ffe0670040abe0076b6153416
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-07-18 18:28:12 +0200

    Creating Home and ImageBanner components + style

---
+++ src/components/Image banner/imageBanner.jsx
@@ -0,0 +1,12 @@
+
+import '../../style/ImageBanner.scss'
+
+
+export function ImageBanner() {
+    return(
+        <div className='container'>
+            <div className='container__overlay'></div>
+                <h1 className='container__title'>Chez vous, partout et ailleurs</h1>
+        </div>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { ImageBanner } from "../components/Image banner/imageBanner"
+import '../style/home.scss'
+
+export function Home() {
+    return(
+        <div className="body">
+            <ImageBanner />
+        </div> 
+    )
+}
