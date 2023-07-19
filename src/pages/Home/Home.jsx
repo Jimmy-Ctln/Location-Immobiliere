a603c1377c7958c142255a3829c18b3345a57ec4
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-07-19 19:49:54 +0200

    Modifying ImageBanner component in Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,14 @@ import { ImageBanner } from "../../components/Image banner/imageBanner"
 import '../../style/home.scss'
 import { Gallery } from "../../components/Gallery/gallery"
 import { Footer } from "../../components/Footer/footer"
+import homeBackground from '../../assets/home-background.svg'
 
 // "Home" displays the "ImageBanner" and "Gallery" components. "Home" will be called with the "/" route in App.jsx
 
 export function Home() {
     return(
         <div className="body">
-            <ImageBanner />
+            <ImageBanner picture={homeBackground} title={'Chez vous, partout et ailleurs'}/>
             <Gallery />
             <Footer />
         </div> 
