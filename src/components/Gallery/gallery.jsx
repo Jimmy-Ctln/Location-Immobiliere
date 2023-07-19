98744a70f732698b69a122aeb57e389bc70c9b9e
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-07-19 17:43:34 +0200

    Addition "data" + modification gallery component

--- src/components/Gallery/gallery.jsx
+++ src/components/Gallery/gallery.jsx
@@ -1,38 +1,34 @@
 import { Card } from "../Card/card";
-import { useEffect, useState } from 'react'
-import '../../style/gallery.scss'
+// import { useEffect, useState } from 'react'
+import "../../style/gallery.scss";
+import { datas } from "../../Datas/datas";
 
 // gallery here allows you to retrieve property rentals and generate them using the card component.
 
 export function Gallery() {
-    const [locationList, setLocationList] = useState([])
+  //   const [locationList, setLocationList] = useState([])
 
-  useEffect(() => {
-    async function fetchPropertyRental() {
-      try {
-        const response = await fetch("http://localhost:3001/api/data")
-        const locationList  = await response.json()
-        setLocationList(locationList)
-      } catch {
-        console.log('Une erreur est survenue');
-      }
-    }
-    fetchPropertyRental();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchPropertyRental() {
+  //     try {
+  //       const response = await fetch("http://localhost:3001/api/data")
+  //       const locationList  = await response.json()
+  //       setLocationList(locationList)
+  //     } catch {
+  //       console.log('Une erreur est survenue');
+  //     }
+  //   }
+  //   fetchPropertyRental();
+  // }, []);
 
   return (
-
     <div className="gallery__container">
-            {locationList.map((location) => (
-            
-                <Card
-                    key={location.id}
-                    cover={location.cover}
-                    title={location.title}
-                />
-            ))}
-
+      {datas.map((location) => (
+        <Card 
+          key={location.id} 
+          cover={location.cover} 
+          title={location.title} />
+      ))}
     </div>
-
-  )
+  );
 }
