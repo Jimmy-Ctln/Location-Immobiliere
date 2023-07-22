35e6711c157fcbe9439a68fdbb76f513080db3dd
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-07-22 13:28:03 +0200

    creation onClick to retrieve the id parameter

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,11 +1,26 @@
 import '../../style/card.scss'
+import { useNavigate } from 'react-router-dom'
 
 // creation of a standard card with props to be used in the gallery component
 
-export function Card({cover, title}) {
+export function Card({id, cover, title}) {
+
+    // I use navigate to sotcker and send the id when I click on a card
+
+    let navigate = useNavigate()
+
+    // With the onClick event, I can store the id of the card here
+
+    const retrieveIdCard = (propertyRentalId) => {
+        // console.log(id)
+        navigate("../PropertyRental/"+ propertyRentalId)
+    }
+
+
+    
     return(
         
-        <div className='card'>
+        <div className='card' id={id} onClick={() => retrieveIdCard(id)}>
             <img className="card__img" src={cover} alt="location immobilière"></img>
                 <div className='card__overlay'>
                     <span className='card__title'>{title}</span>
