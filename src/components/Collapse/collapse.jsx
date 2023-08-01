b3cb20d3ba462d8afa0f31a9bf841301f553fd70
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-08-01 12:27:10 +0200

    Change animation collapse

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -3,7 +3,6 @@ import arrow from "../../assets/arrow.svg";
 import "../../style/collapse.scss";
 
 export function Collapse({ title, content }) {
-  const [isOpen, setIsOpen] = useState(false);
 
 
   const isArray = Array.isArray(content);
@@ -18,34 +17,24 @@ export function Collapse({ title, content }) {
     }
   }
 
-  return isOpen ? (
+  const [isOpen, setIsOpen] = useState(false);
+
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  
+  return (
     <div className="collapse">
-      <div className="collapse__title">
-        {title}
-        <img
-          src={arrow}
-          className="collapse__img collapse__img__animateOn"
-          onClick={() => setIsOpen(false)}
-          alt="flèche animation"
-        />
+      <div className="collapse__title" onClick={handleClick}>{title}
+        <img src={arrow} className={`collapse__img ${isOpen ? 'collapse__img__animateOn' : 'collapse__img__animateOff'}`} alt="flèche animation"/>
       </div>
-      <div className="collapse__container collapse__container__open">
-        <div className="collapse__content collapse__container__open">
+      <div className={`collapse__content ${isOpen ? 'collapse__content__open' : ''}`}>
           {isArray ? CheckIsArray() : content}
-        </div>
       </div>
     </div>
-  ) : (
-    <div className="collapse">
-      <div className="collapse__title">
-        {title}
-        <img
-          src={arrow}
-          className="collapse__img collapse__img__animateOff "
-          onClick={() => setIsOpen(true)}
-          alt="flèche animation"
-        />
-      </div>
-    </div>
-  );
+  )
 }
+
+
--- src/pages/About_Us/AboutUs.jsx
+++ src/pages/About_Us/AboutUs.jsx
@@ -8,29 +8,33 @@ export function AboutUs() {
 
     const collapse = [
         {
+            id: "1",
             title: "Fiabilité",
             content : "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sontrégulièrement vérifiées par nos équipes."
         },
         {
+            id: "2",
             title: "Respect",
             content:"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou deperturbation du voisinage entraînera une exclusion de notre plateforme."
         },
         {
+            id: "3",
             title: "Service",
             content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou deperturbation du voisinage entraînera une exclusion de notre plateforme."
 
         },
         {
+            id: "4",
             title: "Sécurité",
             content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logementcorrespond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'aulocataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisonségalement des ateliers sur la sécurité domestique pour nos hôtes."
         }
     ]
 
     return(
-        <div>
-                <ImageBanner picture={aboutBackground}/>
+        <div className="aboutUs">
+                <ImageBanner className="banner" picture={aboutBackground}/>
             <div className="aboutUs__collapse">
-                {collapse.map(collapse => ( <Collapse key={collapse.title} title={collapse.title} content={collapse.content} />))}
+                {collapse.map(collapse => <Collapse key={collapse.id} title={collapse.title} content={collapse.content} />)}
             </div>
         </div>
     )
