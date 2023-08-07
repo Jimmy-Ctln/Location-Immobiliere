f38bc6a06946290dd1422f09fe2d7fa7a5351ba3
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-08-07 12:09:27 +0200

    Create function next and prev for the carousel

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -8,17 +8,33 @@ export function Carrousel({pictures}) {
 
     const [currImage, setCurrImage] = useState(0)
     const currentSlide = currImage + 1;
-    const currentPictures = pictures.length;
-    const hideElement = currentPictures === 1;
+    const totalPictures = pictures.length;
+    const hideElement = totalPictures === 1;
+
+
+    function prevSlide() {
+        if (currImage === 0) {
+            setCurrImage(pictures.length - 1) 
+        } else {
+            setCurrImage(currImage - 1)
+        }
+    }
     
-    
+    function nextSlide() {
+        if (currImage === pictures.length -1) {
+            setCurrImage(0)
+        } else {
+            setCurrImage(currImage + 1)
+        } 
+    }
+
     return(
             <div className="carrousel">
                     <img className="carrousel__img" src={pictures[currImage]} alt="logement" />
-                    <img src={arrowLeft} className={`carrousel__arrow__left ${hideElement ? 'hide__elements' : ''}`} onClick={() => currImage === 0 ? setCurrImage(pictures.length - 1) : currImage > 0 && setCurrImage(currImage - 1)} alt="Flèche de gauche carrousel" />
-                    <img src={arrowRight} className={`carrousel__arrow__right ${hideElement ? 'hide__elements' : ''}`} onClick={() => currImage === pictures.length -1 ? setCurrImage(0) : setCurrImage(currImage + 1)} alt="Flèche de droite carrousel" />
+                    <img src={arrowLeft} className={`carrousel__arrow__left ${hideElement ? 'hide__elements' : ''}`} onClick={prevSlide} alt="Flèche de gauche carrousel" />
+                    <img src={arrowRight} className={`carrousel__arrow__right ${hideElement ? 'hide__elements' : ''}`} onClick={nextSlide} alt="Flèche de droite carrousel" />
                 <div className={`carrousel__slideCounter ${hideElement ? 'hide__elements' : ''}`}>
-                    <span>{currentSlide}/{currentPictures}</span>
+                    <span>{currentSlide}/{totalPictures}</span>
                 </div>
             </div>
     )
